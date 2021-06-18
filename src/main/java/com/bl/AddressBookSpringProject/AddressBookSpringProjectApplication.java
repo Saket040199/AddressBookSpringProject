f07a114f89bef8a299b506f6fee53bc63ce3b257
saket <saketj420@gmail.com>
2021-06-18 23:29:00 +0530

    Adding all dependencies which are used in spring project

---
+++ src/main/java/com/bl/AddressBookSpringProject/AddressBookSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bl.AddressBookSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bl/AddressBookSpringProject/AddressBookSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bl.AddressBookSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
