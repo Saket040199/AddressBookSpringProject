b971adbf960a2529bf7f76380cd57f39b99eede2
saket <saketj420@gmail.com>
2021-06-24 14:21:14 +0530

    Ability to use lombok for getting logs in project

--- src/main/java/com/bl/AddressBookSpringProject/AddressBookSpringProjectApplication.java
+++ src/main/java/com/bl/AddressBookSpringProject/AddressBookSpringProjectApplication.java
@@ -2,13 +2,19 @@ package com.bl.AddressBookSpringProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookSpringProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookSpringProjectApplication.class, args);
-		System.out.println("Project is Deploying Properly");
+		ApplicationContext context = SpringApplication.run(AddressBookSpringProjectApplication.class, args);
+	
+		log.info("Employee Payroll app started in {} environment",
+				  context.getEnvironment().getProperty("environment"));
 	}
 
 }
